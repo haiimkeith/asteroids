6d06ef5df56bad403a2bff4a356b3005122f6378
haiimkeith <haiimkeith@gmail.com>
2024-09-25 16:41:55 -0700

    add asteroid field with spawning asteroids

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import pygame
+from player import *
 from constants import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     while True:
-        for event in pygame.event.get():
-            if event.type = pygame.QUIT:
-                return
         screen.fill("black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        for upd in updatable:
+            upd.update(dt)
+        for dr in drawable:
+            dr.draw(screen)
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
